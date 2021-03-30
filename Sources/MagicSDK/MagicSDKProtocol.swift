6d1413a9c56920a560a19931720d96e010861747
Nicholas Babo <nickbabo7@gmail.com>
2021-03-30 14:26:12 -0300

    Remove public modifier from protocol

--- Sources/MagicSDK/MagicSDKProtocol.swift
+++ Sources/MagicSDK/MagicSDKProtocol.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public protocol MagicSDKProtocol {
-    public func cards(completion: ((Result<CardResponse, Error>) -> Void))
+    func cards(completion: ((Result<CardResponse, Error>) -> Void))
 }
