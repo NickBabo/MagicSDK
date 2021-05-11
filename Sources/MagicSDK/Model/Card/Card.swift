b748d37aa4a3e3745ca93ef2970f9897564a0af0
Nicholas Babo <nickbabo7@gmail.com>
2021-05-11 15:42:52 -0300

    Add legality enums to Card model

--- Sources/MagicSDK/Model/Card/Card.swift
+++ Sources/MagicSDK/Model/Card/Card.swift
@@ -2,6 +2,20 @@ import Foundation
 
 public struct Card: Decodable {
     public var name: String
-    public var legalities: [String: String]
+    public var legalities: [GameMode: Legality]
     public var set: String
+}
+
+public enum GameMode: String, Decodable {
+    case duel
+    case penny
+    case modern
+    case commander
+    case legacy
+}
+
+public enum Legality: String, Decodable {
+    case legal
+    case notLegal = "not_legal"
+    case banned
 }
